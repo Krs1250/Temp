02a9fc21815ed62d18f3bb6724fc53c0c77aba28
Krs1250 <89281341+Krs1250@users.noreply.github.com>
2022-07-15 22:51:16 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-text = str(input("输入文本："))
-text=text.replace('<',r'<pre>&lt;')
-text=text.replace('>',r'&gt</pre>;')
-text=text.replace('HTML','<b>HTML')
-text=text.replace('元素','元素</b>')
-
-print(text)
